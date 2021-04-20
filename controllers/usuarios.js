4fffd3c1ca91c02a03e2dfe12df81861ac429d0f
Vicente Barra <vicente.barra.94@gmail.com>
2021-04-20 00:05:58 -0400

    BackEnd REST basico

---
+++ controllers/usuarios.js
@@ -0,0 +1,49 @@
+const { response, request } = require('express');
+
+
+const usuariosGet = (req = request, res = response) => {
+    const { q, nombre = 'No name', apikey} = req.query;
+    res.json({
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey
+    });
+}
+
+const usuariosPost =  (req, res) => {
+    const { nombre, edad } = req.body;
+    res.status(201).json({
+        msg: 'post API - controlador',
+        nombre,
+        edad
+    });
+}
+
+const usuariosPut = (req, res) => {
+    const id = req.params.id;
+    res.json({
+        msg: 'put API - controlador',
+        id
+    });
+}
+
+const usuariosPatch =  (req, res) => {
+    res.json({
+        msg: 'patch API - controlador'
+    });
+}
+
+const usuariosDelete = (req, res) => {
+    res.json({
+        msg: 'delete API - controlador'
+    });
+}
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosPatch,
+    usuariosDelete
+}
